089b322f157bdac80041c82a18d24c982eee5a25
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-07-03 22:12:49 +0200

    commit

--- todolistbackend/todolistbackend/settings.py
+++ todolistbackend/todolistbackend/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-#^*@i_bq8874eqc&+09qqswv^^)3fqxhi#5)+!u*fm5a-!)%3&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
+
+CORS_ALLOW_ALL_ORIGINS = ['http://localhost:4200/']
 
 
 # Application definition
@@ -38,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todolist'
+    'rest_framework.authtoken',
+    'todolist',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
