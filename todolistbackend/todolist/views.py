63666816c60ec2732e51382cf733526305eee43d
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-07-07 10:50:21 +0200

    done

--- todolistbackend/todolist/views.py
+++ todolistbackend/todolist/views.py
@@ -3,15 +3,17 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import TodoItem
 from .serializers import TodoItemSerializer
 # Create your views here.
 class TodoItemView(APIView):
-    # authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = []
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-    def post(self, request, format=None):
-        todos = TodoItem.objects.all()
+    def get(self, request, format=None):
+        todos = TodoItem.objects.filter(author=request.user)
         serializer = TodoItemSerializer(todos, many=True)
         return Response(serializer.data)
     
